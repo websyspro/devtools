66918967d7c4650a4ee72aac9ba2df5ad6fe2515
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-08-29 19:34:20 -0300

    Release 1.0.83

--- src/Consts/Hosts.php
+++ src/Consts/Hosts.php
@@ -5,4 +5,6 @@ namespace Websyspro\DevTools\Consts;
 class Hosts
 {
   public static string $hostname = "127.0.0.1";
+  public static int $webSocketPort = 3002;
+  public static int $httpServerPort = 3002;
 }

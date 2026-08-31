36d65dbf4774b2808d92b81c987094f72a577a37
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-08-30 22:14:14 -0300

    Release 1.0.115

--- src/Handlers/BrowserReloadHandler.php
+++ src/Handlers/BrowserReloadHandler.php
@@ -60,44 +60,40 @@ extends EventHandler
 
   private function sendWebSocketProcess(
   ): void {
-    $handlerHost = $this->watchEvents->watchJSON->webSocketHost;
     $handlerPort = $this->watchEvents->watchJSON->webSocketPort;
-
     $handler = @socket_create(
       AF_INET, SOCK_STREAM, SOL_TCP
     );
     
-    if( !$handler ){
+    if( $handler === false ){
       return;
     }
 
     $handlerConected = @socket_connect( 
-      $handler, $handlerHost, $handlerPort
+      $handler, Hosts::$hostname, $handlerPort
     );
     
     if( $handlerConected === false ){
       socket_close( $handler );
-      return;
+    } else {
+      $handlerKey = base64_encode( random_bytes(16) );
+      $handlerResponse = implode(
+        "\r\n", [
+          "GET / HTTP/1.1",
+          "Host: 127.0.0.1:{$handlerPort}",
+          "Upgrade: websocket",
+          "Connection: Upgrade",
+          "Sec-WebSocket-Key: {$handlerKey}",
+          "Sec-WebSocket-Version: 13",
+          "\r\n"
+        ]
+      );
+
+      socket_write( $handler, $handlerResponse );
+      socket_read( $handler, 2048 );
+      socket_write( $handler, $this->encodeWebSocketFrame( "notification-client" ) );
+      socket_close( $handler );
     }
-
-    $key = base64_encode( random_bytes(16) );
-    $handlerResponse = implode(
-      "\r\n", [
-        "GET / HTTP/1.1",
-        "Host: 127.0.0.1:{$handlerPort}",
-        "Upgrade: websocket",
-        "Connection: Upgrade",
-        "Sec-WebSocket-Key: {$key}",
-        "Sec-WebSocket-Version: 13",
-        "\r\n"
-      ]
-    );
-
-    socket_write( $handler, $handlerResponse );
-    socket_read( $handler, 2048 );
-
-    socket_write( $handler, $this->encodeWebSocketFrame( "notification-client" ) );
-    socket_close( $handler );
   }
 
   private function encodeWebSocketFrame(

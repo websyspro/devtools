01480c433c33bf97eb4f942ca62e3d613c0ed48e
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-09-08 18:14:46 -0300

    Release 1.0.142

--- src/Runtimes/http-server-router.php
+++ src/Runtimes/http-server-router.php
@@ -7,39 +7,57 @@
  * Não deve ser exportado como binário do Composer.
  * Usado pelo BrowserReloadHandler para rodar em processo separado.
  */
-
 defined( "DevTools_Base_Dir" ) || define(
   "DevTools_Base_Dir", realpath(
     dirname( __DIR__, 5 ) 
   ) . DIRECTORY_SEPARATOR
 );
 
+
+/*
+ * Load Autoload
+ * */
 require_once DevTools_Base_Dir . "vendor/autoload.php";
 use Websyspro\DevTools\Middlewares\HttpServerRouter;
 use Websyspro\DevTools\Objects\RequestHandler;
+use Websyspro\DevTools\Interfaces\DevTools;
 
-$httpServerRouter = new HttpServerRouter(
-  directorys: [ "core", "public" ], 
-  friendlyUrl: true 
-);
+/**
+ * Load devTools
+ */
+$devTools = require_once DevTools_Base_Dir . "devTools.php";
 
-$handlerResponse = $httpServerRouter->handlerResponse();
-if( $handlerResponse->empty() === false ){
-  [ $requestHandler ] = $handlerResponse->toArray();
+/**
+ * Validação devTools
+ */
+if( $devTools instanceof DevTools ){
+  $httpServerRouter = new HttpServerRouter(
+    directorys: [ 
+      ...[ $devTools->documentRoot ],
+      ...$devTools->includes
+    ], friendlyUrl: true 
+  );
 
-  if( $requestHandler instanceof RequestHandler ){
-    if( $requestHandler->requestTarget->pathInfos->isStatic()){
-      $requestHandler->requestTarget->pathInfos->handlerStaticResponse();
-    } else {
-      try {
-        ob_start();
-        require_once $requestHandler->requestTarget->pathInfos->file->name;
-        exit( ob_get_contents());
-      } catch ( Throwable $throwable ){
-        $httpServerRouter->error( $throwable );
+  $handlerResponse = $httpServerRouter->handlerResponse();
+  if( $handlerResponse->empty() === false ){
+    [ $requestHandler ] = $handlerResponse->toArray();
+
+    if( $requestHandler instanceof RequestHandler ){
+      if( $requestHandler->requestTarget->pathInfos->isStatic()){
+        $requestHandler->requestTarget->pathInfos->handlerStaticResponse();
+      } else {
+        try {
+          ob_start();
+          require_once $requestHandler->requestTarget->pathInfos->file->name;
+          exit( ob_get_contents());
+        } catch ( Throwable $throwable ){
+          $httpServerRouter->error( $throwable );
+        }
       }
     }
+  } else {
+    $httpServerRouter->notFound();
   }
 } else {
-  $httpServerRouter->notFound();
+  echo "not devTools";
 }

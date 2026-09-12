ee5fa1c1052a1b49537e34aaeb55096477b62ead
Emerson Thiago Sousa Albuquerque <e.thiago-pla@platos.parceirosedu.com.br>
2026-09-12 17:38:10 -0300

    Release 1.0.148

--- src/WatchEvents.php
+++ src/WatchEvents.php
@@ -169,12 +169,6 @@ class WatchEvents
       );
     }
 
-    if( empty( $this->devTools->scriptName )){
-      throw new RuntimeException(
-        "No scripts registered to watch"
-      );
-    }
-
     $this->scanFilesPrevious();
     $this->dispatchEvent(
       DispatchType::Started
